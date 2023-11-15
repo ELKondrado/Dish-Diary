15c26643cbed304288bc1dac05e44eb4f631bf15
ELKondrado <condrat.radu69@yahoo.com>
2023-11-15 19:07:11 +0200

    post and query to find recipe by name

--- src/main/java/com/example/demo/Recipe/RecipeService.java
+++ src/main/java/com/example/demo/Recipe/RecipeService.java
@@ -3,9 +3,12 @@ package com.example.demo.Recipe;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class RecipeService {
     private final RecipeRepository recipeRepository;
@@ -18,5 +21,13 @@ public class RecipeService {
     @GetMapping
     public List<Recipe> getRecipes(){
         return recipeRepository.findAll();
+    }
+
+    public void addNewRecipe(Recipe recipe) {
+        Optional<Recipe> recipeOptional = recipeRepository.findRecipeByName(recipe.getName());
+        if(recipeOptional.isPresent()){
+            throw new IllegalStateException("Name taken");
+        }
+        recipeRepository.save(recipe);
     }
 }
