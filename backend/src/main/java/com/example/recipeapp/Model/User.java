45cf16cbcd8b7757a8e6ec695121605ca4980656
rares <rares.iacob@student.upt.ro>
2023-12-14 14:30:20 +0200

    fixed errors from register-form.component.ts

--- backend/src/main/java/com/example/recipeapp/Model/User.java
+++ backend/src/main/java/com/example/recipeapp/Model/User.java
@@ -32,5 +32,9 @@ public class User {
     @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
         inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
     private List<Role> roles = new ArrayList<>();
+
+    public User() {
+
+    }
 }
 
